dd9fe50927cc4a2e2bde81d049d3666d10b6cdc6
sastra12 <anonymousonly594@gmail.com>
2022-12-05 16:07:40 +0700

    show error message on login view admin

--- resources/views/admin/login.blade.php
+++ resources/views/admin/login.blade.php
@@ -37,6 +37,15 @@
 
                                     </div>
                                     <div class="form-body">
+                                        @if ($errors->any())
+                                            <div class="alert alert-danger">
+                                                <ul>
+                                                    @foreach ($errors->all() as $error)
+                                                        <li>{{ $error }}</li>
+                                                    @endforeach
+                                                </ul>
+                                            </div>
+                                        @endif
                                         <form class="row g-3" method="POST" action="{{ route('login') }}">
                                             @csrf
                                             <div class="col-12">
