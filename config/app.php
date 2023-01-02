621f3821bd52dc5560db0fa40c645b6e0b40d7f4
sastra12 <anonymousonly594@gmail.com>
2023-01-02 16:23:49 +0700

    install image intervention package

--- config/app.php
+++ config/app.php
@@ -166,6 +166,7 @@ return [
         /*
          * Package Service Providers...
          */
+        Intervention\Image\ImageServiceProvider::class,
 
         /*
          * Application Service Providers...
@@ -231,6 +232,7 @@ return [
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
         'DataTables' => Yajra\DataTables\Facades\DataTables::class,
+        'Image' => Intervention\Image\Facades\Image::class
 
     ],
 
